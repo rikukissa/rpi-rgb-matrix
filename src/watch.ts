be2f1452018ab03687345aedbb96fe2983abb06b
Riku Rouvila <riku.rouvila@gmail.com>
2022-02-13 12:23:09 +0200

    remove date-fns

--- src/watch.ts
+++ src/watch.ts
@@ -2,7 +2,6 @@ require("dotenv").config()
 import axios from "axios"
 import { createCanvas, registerFont } from "canvas"
 import { join } from "path"
-import { format, endOfMinute } from "date-fns"
 import { removeAlpha } from "./util"
 
 const DRAW_ENDPOINT = process.env.DRAW_ENDPOINT!
@@ -10,6 +9,18 @@ const DRAW_ENDPOINT = process.env.DRAW_ENDPOINT!
 registerFont(join(__dirname, "../thin_pixel-7.ttf"), {
   family: "VT323",
 })
+
+function endOfMinute(date: Date) {
+  const newDate = new Date(date)
+  newDate.setMinutes(newDate.getMinutes() + 1)
+  newDate.setSeconds(0)
+  newDate.setMilliseconds(-1)
+  return newDate
+}
+
+function format(date: Date) {
+  return `${date.getHours() % 12}:${date.getMinutes()}`
+}
 
 async function main() {
   const canvas = createCanvas(32, 32)
@@ -21,7 +32,7 @@ async function main() {
   ctx.textBaseline = "middle"
 
   const date = new Date()
-  const dateString = format(date, "h:mm")
+  const dateString = format(date)
 
   ctx.fillStyle = "#fff"
   ctx.translate(16, 16)
