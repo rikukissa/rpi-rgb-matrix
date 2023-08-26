4d2d521e5ce9bc35bc65d162489ed3fc93ed4b41
Riku Rouvila <riku.rouvila@gmail.com>
2023-08-26 09:36:31 +0300

    fix index.html path

--- src/index.ts
+++ src/index.ts
@@ -116,7 +116,7 @@ async function clientHandler(
   res: http.ServerResponse
 ) {
   res.setHeader("Content-Type", "text/html")
-  res.write(readFileSync(join(__dirname, "../index.html")))
+  res.write(readFileSync(join(__dirname, "../../index.html")))
   res.end()
   return
 }
